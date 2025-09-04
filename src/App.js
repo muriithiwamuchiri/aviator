604e5388d679f22b1e8053cef920216d453d50ca
muriithiwamuchiri <muriithiwagatuku@gmail.com>
2025-09-04 17:51:58 +0300

    Replace src/App.js with updated content.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './components/Game';
+import './styles/theme.css';
+
+function App() {
+  return (
+    <div className="app-green">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
